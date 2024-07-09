d62225184e2acef4bad4b54d2c3e9c325e9b049a
bettercodevivek <viveksingh0520@gmail.com>
2024-07-09 16:58:46 +0530

    Creation of Homepage and addition of routing to main and layout file

--- BookWeb/src/main.jsx
+++ BookWeb/src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import { RouterProvider,createBrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import Layout from './Layout';
+import Home from './Components/Home';
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Layout/>,
+    children:[
+      {
+        index:true,
+        element:<Home/>
+      },
+      {
+        path:'home',
+        element:<Home/>
+      },
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )

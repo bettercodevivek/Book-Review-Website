2346ebc50b06c6f7a4d0237365208467a13e4662
bettercodevivek <viveksingh0520@gmail.com>
2024-07-10 13:32:26 +0530

    Updation in navbar component

--- BookWeb/src/Components/Navbar.jsx
+++ BookWeb/src/Components/Navbar.jsx
@@ -15,8 +15,13 @@ export default function Navbar() {
                 <div className="flex items-center justify-between mx-auto max-w-screen-xl">
                     <Link to="/" className="flex items-center">
                         <div className="hidden md:flex flex-col items-start ml-4">
+                            <div className="flex flex-row">
+                            <img className="h-12 px-4" src='https://i.postimg.cc/43SDC78P/book-opened-svgrepo-com.png'></img>
+                            <div className="flex flex-col items-left">
                             <div className="text-lg font-semibold text-gray-900">Reader's Realm</div>
-                            <div className="text-sm text-gray-600">"Your Harbor for Honest Reviews"</div>
+                            <div className="text-sm text-gray-600">Your Harbor for Honest Reviews</div>
+                            </div>
+                            </div>
                         </div>
                     </Link>
 
@@ -85,12 +90,12 @@ export default function Navbar() {
 
             <div className={`fixed inset-0 z-40 transition-opacity ${isSidebarOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
                 <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-filter backdrop-blur-sm" onClick={toggleSidebar}></div>
-                <div className={`fixed inset-y-0 left-0 w-64 bg-white bg-opacity-70 backdrop-filter backdrop-blur-lg shadow-lg transform transition-transform duration-300 ease-in-out ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'} rounded-r-xl`}>
+                <div className={`fixed inset-y-0 left-0 w-64 bg-blue-400 bg-opacity-50 backdrop-filter backdrop-blur-lg shadow-lg transform transition-transform duration-300 ease-in-out ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'} rounded-r-xl`}>
                     <div className="flex justify-between items-center p-4">
                         <button
                             onClick={toggleSidebar}
                             type="button"
-                            className="text-gray-500 hover:text-gray-700 focus:outline-none focus:text-gray-700"
+                            className="text-black hover:text-gray-700 focus:outline-none focus:text-gray-700"
                             aria-label="close menu"
                         >
                             <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -110,43 +115,43 @@ export default function Navbar() {
                             </NavLink>
                         </li>
                         <li>
-                            <NavLink to='/profile'
+                            <NavLink to='/dashboard'
                                 onClick={toggleSidebar}
                                 className={({ isActive }) =>
                                     `flex items-center space-x-4 py-2 px-6 rounded-lg shadow-md bg-white text-lg font-semibold ${isActive ? "text-blue-500" : "text-gray-900"} hover:bg-gray-200 hover:text-blue-700 transition-transform transform hover:scale-105`
                                 }
                             >
-                                <FiUser className="w-5 h-5" /> <span>Corporate Profile</span>
+                                <FiUser className="w-5 h-5" /> <span>Dashboard</span>
                             </NavLink>
                         </li>
                         <li>
-                            <NavLink to='/vision'
+                            <NavLink to='/books'
                                 onClick={toggleSidebar}
                                 className={({ isActive }) =>
                                     `flex items-center space-x-4 py-2 px-6 rounded-lg shadow-md bg-white text-lg font-semibold ${isActive ? "text-blue-500" : "text-gray-900"} hover:bg-gray-200 hover:text-blue-700 transition-transform transform hover:scale-105`
                                 }
                             >
-                                <FiEye className="w-5 h-5" /> <span>Our Vision</span>
+                                <FiEye className="w-5 h-5" /> <span>Books</span>
                             </NavLink>
                         </li>
                         <li>
-                            <NavLink to='/products'
+                            <NavLink to='/guideline'
                                 onClick={toggleSidebar}
                                 className={({ isActive }) =>
                                     `flex items-center space-x-4 py-2 px-6 rounded-lg shadow-md bg-white text-lg font-semibold ${isActive ? "text-blue-500" : "text-gray-900"} hover:bg-gray-200 hover:text-blue-700 transition-transform transform hover:scale-105`
                                 }
                             >
-                                <FiBox className="w-5 h-5" /> <span>Our Products</span>
+                                <FiBox className="w-5 h-5" /> <span>Guidelines</span>
                             </NavLink>
                         </li>
                         <li>
-                            <NavLink to='/contact'
+                            <NavLink to='/login'
                                 onClick={toggleSidebar}
                                 className={({ isActive }) =>
                                     `flex items-center space-x-4 py-2 px-6 rounded-lg shadow-md bg-white text-lg font-semibold ${isActive ? "text-blue-500" : "text-gray-900"} hover:bg-gray-200 hover:text-blue-700 transition-transform transform hover:scale-105`
                                 }
                             >
-                                <FiMail className="w-5 h-5" /> <span>Contact Us</span>
+                                <FiMail className="w-5 h-5" /> <span>Login</span>
                             </NavLink>
                         </li>
                     </ul>
